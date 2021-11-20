2a0c167476ab741fc014089f8e31bebe5dbea22b
NUTTHARIKA THONGTHAM <89317482+NuttharikaTht@users.noreply.github.com>
2021-11-21 03:21:52 +0700

    Add files via upload

---
+++ build/generated-sources/ap-source-output/Model/Student_.java
@@ -0,0 +1,15 @@
+package Model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-21T02:19:08")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, String> name;
+    public static volatile SingularAttribute<Student, Double> gpa;
+    public static volatile SingularAttribute<Student, String> id;
+
+}
---
+++ src/java/Controller/Remove.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Student;
+import Model.StudentTable;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aomms
+ */
+public class Remove extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        Student std = new Student();
+        std.setId(request.getParameter("id"));
+        std.setName(request.getParameter("name"));
+        std.setGpa(Double.parseDouble(request.getParameter("gpa")));
+        StudentTable.removeStudent(std);
+        
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Remove</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>"+"Remove Success"+"</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        /*request.setAttribute("caller", "/StudentDB/Remove.jsp");
+        String uri = (String)request.getAttribute("caller");
+        getServletContext().getRequestDispatcher(uri).forward(request, response);*/
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        String connectionURL = "jdbc:derby://localhost:1527/Student [app on APP]";
+        Connection connection;
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
